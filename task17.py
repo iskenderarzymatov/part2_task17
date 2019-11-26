34a6e7eed976b712da309cf76c70ee927687ba74
iskenderarzymatov <iskenderarzymatov@gmail.com>
2019-11-26 08:54:30 -0500

    part2_task17

---
+++ task17.py
@@ -0,0 +1,4 @@
+ir_number = int(input('Введите первое число: '))
+sec_number = int(input('Введите второе число: '))
+thi_number = int(input('Введите третье число: '))
+print("Сумма: ", fir_number + sec_number + thi_number)
